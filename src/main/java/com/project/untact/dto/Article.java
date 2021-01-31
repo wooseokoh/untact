1952c4c10be85ef0ff4a2eabb7c77e13b9521aca
wooseokoh <allsafe1206@gmail.com>
2021-01-31 16:55:41 +0900

    사용자게시물컨트롤러 추가

---
+++ src/main/java/com/project/untact/dto/Article.java
@@ -0,0 +1,55 @@
+package com.project.untact.dto;
+
+public class Article {
+
+	private int id;
+	private String reDate;
+	private String title;
+	private String body;
+	
+	public Article(int i, String string, String string2, String string3) {
+		this.id = id;
+		this.reDate = reDate;
+		this.title = title;
+		this.body = body;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getReDate() {
+		return reDate;
+	}
+
+	public void setReDate(String reDate) {
+		this.reDate = reDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", reDate=" + reDate + ", title=" + title + ", body=" + body + "]";
+	}
+	
+
+}
