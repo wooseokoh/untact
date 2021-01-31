baeba7f304b50430928dcde699d576657a2e13b0
wooseokoh <allsafe1206@gmail.com>
2021-01-31 21:08:12 +0900

    Util.mapOf 적용

--- src/main/java/com/project/untact/controller/UsrArticleController.java
+++ src/main/java/com/project/untact/controller/UsrArticleController.java
@@ -46,13 +46,8 @@ public class UsrArticleController {
 		String updateDate = regDate;
 		
 		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
-		
-		Map<String, Object> rs = new HashMap<>();
-		rs.put("resultCode", "S-1");
-		rs.put("msg", "성공하였습니다.");
-		rs.put("id", articlesLastId);
-		
-		return rs;
+
+		return Util.mapOf("resultCode", "S-1","msg", "성공하였습니다.","id", articlesLastId);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
@@ -62,17 +57,11 @@ public class UsrArticleController {
 		
 		Map<String, Object> rs = new HashMap<>();
 		
-		if (deleteArticleRs) {
-			rs.put("resultCode", "S-1");
-			rs.put("msg", "성공하였습니다.");			
-		}else {
-			rs.put("resultCode", "F-1");
-			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
+		if (deleteArticleRs == false) {
+			return Util.mapOf("resultCode", "F-1","msg", "해당 게시물은 존재하지 않습니다.");
 		}
 		
-		rs.put("id", id);
-		
-		return rs;
+		return Util.mapOf("resultCode", "S-1","msg", "성공하였습니다.","id", id);
 	}
 
 	private boolean deleteArticle(int id) {
@@ -100,19 +89,14 @@ public class UsrArticleController {
 		Map<String, Object> rs = new HashMap<>();
 		
 		if (selArticle == null) {
-			rs.put("resultCode", "F-1");
-			rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));			
+			return Util.mapOf("resultCode", "F-1","msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
 		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 		
-		rs.put("resultCode", "S-1");
-		rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
-		rs.put("id", id);
-		
-		return rs;
+		return Util.mapOf("resultCode", "S-1","msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
 
 	}
 }
