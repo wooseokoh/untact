673c5661bf7566d2ded94a80dcc8e194a318dbcb
wooseokoh <allsafe1206@gmail.com>
2021-02-13 23:05:31 +0900

    회원정보수정

--- src/main/java/com/project/untact/controller/UsrMemberController.java
+++ src/main/java/com/project/untact/controller/UsrMemberController.java
@@ -97,4 +97,21 @@ public class UsrMemberController {
 
 		return new ResultData("S-1", "로그아웃 되었습니다.");
 	}
+	
+	@RequestMapping("/usr/member/doModify")
+	@ResponseBody
+	public ResultData doModify(@RequestParam Map<String, Object> param, HttpSession session) {
+		if (session.getAttribute("loginedMemberId") == null) {
+			return new ResultData("F-1", "로그인 후 이용해주세요.");
+		}
+
+		if (param.isEmpty()) {
+			return new ResultData("F-2", "수정할 정보를 입력해주세요.");
+		}
+
+		int loginedMemberId = (int) session.getAttribute("loginedMemberId");
+		param.put("id", loginedMemberId);
+
+		return memberService.modifyMember(param);
+	}
 }
--- src/main/java/com/project/untact/service/MemberService.java
+++ src/main/java/com/project/untact/service/MemberService.java
@@ -33,4 +33,10 @@ public class MemberService {
 		return memberDao.getMemberByLoginId(loginId);
 	}
 
+	public ResultData modifyMember(Map<String, Object> param) {
+		memberDao.modifyMember(param);
+
+		return new ResultData("S-1", "회원정보가 수정되었습니다.");
+	}
+
 }
