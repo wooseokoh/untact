373393a719c3d2720f989d2b4539e35402b6b65e
wooseokoh <allsafe1206@gmail.com>
2021-02-13 21:10:01 +0900

    검색어가 없을때는 결과가 안나오는 버그 수정

--- src/main/java/com/project/untact/controller/UsrArticleController.java
+++ src/main/java/com/project/untact/controller/UsrArticleController.java
@@ -46,6 +46,10 @@ public class UsrArticleController {
 			searchKeyword = searchKeyword.trim();
 		}
 		
+		if ( searchKeyword == null ) {
+			searchKeywordType = null;
+		}
+		
 		return articleService.getArticles(searchKeywordType, searchKeyword);
 	}
 
