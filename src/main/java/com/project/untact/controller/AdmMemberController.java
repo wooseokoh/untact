2597b895c8df589ecbbc45f1f4d74ac04c5cacb5
wooseokoh <allsafe1206@gmail.com>
2021-02-22 23:06:52 +0900

    관리자페이지 로그인페이지 구현

--- src/main/java/com/project/untact/controller/AdmMemberController.java
+++ src/main/java/com/project/untact/controller/AdmMemberController.java
@@ -21,6 +21,11 @@ public class AdmMemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	@RequestMapping("/adm/member/login")
+	public String login() {
+		return "adm/member/login";
+	}
+	
 	@RequestMapping("/adm/member/doLogin")
 	@ResponseBody
 	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
