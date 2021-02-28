3c2099205702f2b49414388eb0b9b5c4c85b1d90
wooseokoh <allsafe1206@gmail.com>
2021-02-28 21:53:08 +0900

    MemberDao의 getMember 메서드 오류 수정, 로그인 처리시 관리자 홈으로 이동

--- src/main/java/com/project/untact/controller/AdmMemberController.java
+++ src/main/java/com/project/untact/controller/AdmMemberController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.project.untact.dto.Member;
 import com.project.untact.dto.ResultData;
 import com.project.untact.service.MemberService;
+import com.project.untact.util.Util;
 
 @Controller
 public class AdmMemberController {
@@ -28,32 +29,34 @@ public class AdmMemberController {
 	
 	@RequestMapping("/adm/member/doLogin")
 	@ResponseBody
-	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
+	public String doLogin(String loginId, String loginPw, HttpSession session) {
 		if (loginId == null) {
-			return new ResultData("F-1", "loginId를 입력해주세요.");
+			return Util.msgAndBack("loginId를 입력해주세요.");
 		}
 
 		Member existingMember = memberService.getMemberByLoginId(loginId);
 
 		if (existingMember == null) {
-			return new ResultData("F-2", "존재하지 않는 로그인아이디 입니다.", "loginId", loginId);
+			return Util.msgAndBack("존재하지 않는 로그인아이디 입니다.");
 		}
 
 		if (loginPw == null) {
-			return new ResultData("F-1", "loginPw를 입력해주세요.");
+			return Util.msgAndBack("loginPw를 입력해주세요.");
 		}
 
 		if (existingMember.getLoginPw().equals(loginPw) == false) {
-			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+			return Util.msgAndBack("비밀번호가 일치하지 않습니다.");
 		}
 
 		if ( memberService.isAdmin(existingMember) == false ) {
-			return new ResultData("F-4", "관리자만 접근할 수 있는 페이지 입니다.");
+			return Util.msgAndBack("관리자만 접근할 수 있는 페이지 입니다.");
 		}
 
 		session.setAttribute("loginedMemberId", existingMember.getId());
+		
+		String msg = String.format("%s님 환영합니다.", existingMember.getNickname());
 
-		return new ResultData("S-1", String.format("%s님 환영합니다.", existingMember.getNickname()));
+		return Util.msgAndReplace(msg, "../home/main");
 	}
 	
 	@RequestMapping("/adm/member/doModify")
